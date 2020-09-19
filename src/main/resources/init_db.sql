4ca55c56b58067da433c88cf8a4ef53b0be150a2
Leonid <rommel@ukr.net>
2020-09-19 15:40:51 +0300

    filling data added to init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -13,4 +13,9 @@ CREATE TABLE public.products
     PRIMARY KEY (id)
 );
 
+INSERT INTO products(name, price)
+VALUES ('BMW 320 I', '50000'),
+       ('BMW 530 D', '75000'),
+       ('BMW X5M50D', '110000');
+
 DROP DATABASE bmw_shop;
