d838802031dee3cab4870074d09b5e11e1f0733f
Leonid <rommel@ukr.net>
2020-09-24 14:40:07 +0300

    db.properties created

--- src/main/java/com/internet/shop/util/ConnectionUtil.java
+++ src/main/java/com/internet/shop/util/ConnectionUtil.java
@@ -1,13 +1,15 @@
 package com.internet.shop.util;
 
 import com.internet.shop.exception.DataProcessingException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL
-            = "jdbc:postgresql://localhost:5432/bmw_shop?user=postgres&password=212069";
+    private static final String URL = "jdbc:postgresql://localhost:5432/bmw_shop?";
 
     static {
         try {
@@ -19,8 +21,13 @@ public class ConnectionUtil {
     }
 
     public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL);
+        Properties properties = new Properties();
+        try (InputStream inputStream = ConnectionUtil.class
+                .getClassLoader().getResourceAsStream("db.properties")) {
+            properties.load(inputStream);
+            return DriverManager.getConnection(URL, properties);
+        } catch (IOException e) {
+            throw new DataProcessingException("Can not get file db.properties", e);
         } catch (SQLException e) {
             throw new DataProcessingException("Connection to DB failed", e);
         }
